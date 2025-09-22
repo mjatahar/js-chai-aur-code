c4be9d37b2bcebaef4510fd993af54938a88c4b3
Athar Jawed <mjatahar700@gmail.com>
2025-09-22 23:35:05 +0530

    Update 01_Variables.js

--- 01_basics/01_Variables.js
+++ 01_basics/01_Variables.js
@@ -6,7 +6,7 @@ let accountState;
 
 //accountId=2 //not allowed
 accountEmail="mj@yahoo.com"
-accountPassword="2019"
+accountPassword="x123z"
 accountCity="patna"
 
 console.log(accountId);
@@ -14,4 +14,5 @@ console.log(accountId);
 prefer not to use var
 because of issue in block scope ({block scope}) and functional scope
 */
+
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
